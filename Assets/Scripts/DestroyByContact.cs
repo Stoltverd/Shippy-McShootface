e48cb5e82df499d3a80a8ddfefaede4be2f4fcdd
Esteban Martínez Villegas <esteban.m.v@gmail.com>
2020-08-22 15:23:29 -0500

    Initial push

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, GetComponent<Transform>().position, GetComponent<Transform>().rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.GetComponent<Transform>().position, other.GetComponent<Transform>().rotation);
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
