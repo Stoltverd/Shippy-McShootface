9ca8bd79efde84bb2dbd4f1d043b12c4337160c6
Negroespumoso <68342813+Negroespumoso@users.noreply.github.com>
2020-08-22 17:13:09 -0500

    Pooler creado
    Dentro de la escena hay un controlador hecho para que cree los objetos en start y los meta en un pool en vez de instanciar y destruir cada objeto cada vez que se necesite, se guarda en una lista el objeto para re usarlo y ahorrar rendimiento

---
+++ Assets/Scripts/Game Controllers/Pooler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour
+{
+    //Make this a singleton
+    public static Pooler Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    //Pool class
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDict;
+
+    void Start()
+    {
+        poolDict = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objPool = new Queue<GameObject>();
+
+            //Instantiate objects in pool
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objPool.Enqueue(obj);
+            }
+            // Add pool to dictionary
+            poolDict.Add(pool.tag, objPool);
+        }
+    }
+
+    //This function activates objects
+    public GameObject SpawnFromPool (string tag, Vector3 location, Quaternion rotation)
+    {
+        GameObject spawnableObject = poolDict[tag].Dequeue();
+
+        spawnableObject.SetActive(true);
+        spawnableObject.transform.position = location;
+        spawnableObject.transform.rotation = rotation;
+
+        poolDict[tag].Enqueue(spawnableObject);
+
+        return spawnableObject;
+    }
+}
