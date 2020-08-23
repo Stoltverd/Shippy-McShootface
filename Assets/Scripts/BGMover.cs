6038e1384ba67606bb9d473336a263848ff9f062
Negroespumoso <68342813+Negroespumoso@users.noreply.github.com>
2020-08-22 23:54:25 -0500

    Movimiento seamless del fondo

---
+++ Assets/Scripts/BGMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BGMover : MonoBehaviour
+{
+    Vector3 direction;
+    [SerializeField]
+    GameObject returnPoint;
+    float distance;
+
+    void Start()
+    {
+        direction = new Vector3(0, -2, 0);
+    }
+
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime);
+        distance = Vector3.Distance(transform.position, returnPoint.transform.position);
+        if (distance > 59.8f)
+        {
+            transform.position = returnPoint.transform.position;
+        }
+    }
+}
