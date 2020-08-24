c4f88859d2525a83d74c4ae8b238d5a1e62990da
Esteban Martínez Villegas <esteban.m.v@gmail.com>
2020-08-23 20:10:34 -0500

    Update BGScroller.cs
    Bugfixing

--- Assets/Scripts/BGScroller.cs
+++ Assets/Scripts/BGScroller.cs
@@ -11,7 +11,7 @@ public class BGScroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
