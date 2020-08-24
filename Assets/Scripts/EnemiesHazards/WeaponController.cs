df02cb138b1938f8fb074ac690c51a4831225039
Negroespumoso <68342813+Negroespumoso@users.noreply.github.com>
2020-08-24 18:20:43 -0500

    fixed shoot bug

--- Assets/Scripts/EnemiesHazards/WeaponController.cs
+++ Assets/Scripts/EnemiesHazards/WeaponController.cs
@@ -11,22 +11,24 @@ public class WeaponController : MonoBehaviour
 
     private AudioSource audioSource;
     private Pooler pooler;
+
     // Start is called before the first frame update
     void Start()
     {
+        delay = 0;
         audioSource = GetComponent<AudioSource>();
-        if (gameObject.activeSelf)
-        {
-            InvokeRepeating("Fire", delay, fireRate);
-        }
-
         pooler = Pooler.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        delay += Time.deltaTime;
+        if (delay >= fireRate)
+        {
+            Fire();
+            delay = 0;
+        }
     }
 
     void Fire()
