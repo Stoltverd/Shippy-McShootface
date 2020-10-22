e98be17631f7cf7e6c5f0838daa2533bf69ba276
Esteban Martínez Villegas <esteban.m.v@gmail.com>
2020-10-22 00:55:48 -0500

    Save system redi

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void NewGame ()
+    public void StartGame ()
     {
         SceneManager.LoadScene("Main");
     }
@@ -13,19 +13,5 @@ public class MainMenu : MonoBehaviour
     public void QuitGame ()
     {
         Application.Quit();
-    }
-
-    public void LoadGame()
-    {
-        PlayerData data = SaveSystem.LoadPlayer();
-
-        GameManager.money = data.money;
-        GameManager.player.GetComponent<PlayerManager>().health = data.health;
-        //Load position
-        Vector3 position;
-        position.x = data.position[0];
-        position.y = data.position[1];
-        position.z = data.position[2];
-        GameManager.player.transform.position = position;
     }
 }
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -8,7 +8,11 @@ public class PlayerData
 
     public int money;
     public float health;
-
+    public float speed;
+    public int misiles;
+    public int maxMisiles;
+    public float boost;
+    public float maxBoost;
     public float[] position; //Can't use Vector3. Can't be serialized.
 
 
@@ -16,7 +20,12 @@ public class PlayerData
     public PlayerData(GameManager gameManager,PlayerManager playerManager, PlayerMovement playerMovement)
     {
         money = gameManager.money;
-        //health = playerManager.health;
+        health = playerManager.health;
+        speed = playerMovement.speed;
+        misiles = gameManager.misiles;
+        maxMisiles = gameManager.maxMisiles;
+        boost = playerMovement.currentBoost;
+        maxBoost = gameManager.maxBoost;
 
         position = new float[3];
         position[0] = playerMovement.transform.position.x;
