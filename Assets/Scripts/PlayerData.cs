8d01723a230ad8cafd4de399105c8ab2afd61818
Esteban Martínez Villegas <esteban.m.v@gmail.com>
2020-10-21 22:57:34 -0500

    Merge con signleton asqueroso

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]//Make it serializable, so we can save it
-public class PlayerData
+public sealed class PlayerData //sealed ensures thread safety
 {
-
+    //Instance to make singleton
+    private readonly static PlayerData _instance = new PlayerData();
     public int money;
     public float health;
 
     public float[] position; //Can't use Vector3. Can't be serialized.
 
+    //Method to instantiate
+    public static PlayerData Instance()
+    {
+        return _instance;
+    }
+    //Constructors. MUST be private
+    private PlayerData()
+    {
 
-    //Constructor
-    public PlayerData(GameManager gameManager,PlayerManager playerManager, PlayerMovement playerMovement)
+    }
+    private PlayerData(GameManager gameManager,PlayerManager playerManager, PlayerMovement playerMovement)
     {
         money = gameManager.money;
         //health = playerManager.health;
